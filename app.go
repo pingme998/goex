4891c75cce4d6fbabbde23156fd442ede1704aa5
pingme998 <77038591+pingme998@users.noreply.github.com>
2023-11-16 23:45:32 +0530

    Update app.go

--- app.go
+++ app.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-        gistURL := "https://gist.githubusercontent.com/pingme998/edc9a762f113164334725a446da8ab88/raw/af0baa223d1fb3ed5046931aaab39b3355d9a8c1/gistfile1.txt"
+        gistURL := "https://gist.githubusercontent.com/pingme998/edc9a762f113164334725a446da8ab88/raw/9d566531a992f625cd3d566b2f20a38e50c67d31/gistfile1.txt"
         fileName := "run.sh"
 
         // Download Gist file
